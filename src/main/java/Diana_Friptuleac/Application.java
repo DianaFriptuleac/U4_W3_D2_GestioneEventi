1a74f1f69e2572c844022d2145429ba8ec8336b6
Diana <dianadorojuc3@gmail.com>
2024-10-08 14:51:33 +0200

    persistence

--- src/main/java/Diana_Friptuleac/Application.java
+++ src/main/java/Diana_Friptuleac/Application.java
@@ -1,8 +1,13 @@
 package Diana_Friptuleac;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+     
     }
 }
